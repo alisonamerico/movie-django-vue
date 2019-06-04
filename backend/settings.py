08c19df434604ea1605a80fd908f4bda7a99bd09
alison <alison@metanoia>
2019-06-04 20:30:33 -0300

    Extraída configuração de SECRET_KEY
    close #22

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2lrsh5i1w!4!@ja6c+4c*&ya+9rphj7_xfb4w46^g$710#b#ks'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
