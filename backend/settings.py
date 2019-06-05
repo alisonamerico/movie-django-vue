eeed8bf0b26ff222cb5c87a3acd7975425bae284
alison <alison@metanoia>
2019-06-04 22:14:53 -0300

    Instalado psycopg2 e testar Postgresql no Travis
    close #30

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 from decouple import config, Csv
 from functools import partial
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
