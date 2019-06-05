a6eec4cd08d30435a26741489f9224def24bc9c6
alison <alison@metanoia>
2019-06-05 15:01:02 -0300

    Criado e configurado model e admin da app movie
    close #44

---
+++ backend/movie/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=120)
+    genres = models.CharField(max_length=120)
+    duration = models.IntegerField()
+    title_year = models.IntegerField()
+    director_name = models.CharField(max_length=120)
+    imdb_score = models.DecimalField(max_digits=2, decimal_places=1)
+    picture = models.FileField()
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    def __str__(self):
+        return self.movie_title  # pragma: no cover
