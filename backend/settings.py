df7867ec912f6fb7ed40d57388aa0d90dd88289a
alison <alison@metanoia>
2019-06-05 13:23:20 -0300

    Instalado e configurado o django-cors-headers
    close #42

--- backend/settings.py
+++ backend/settings.py
@@ -43,9 +43,12 @@ INSTALLED_APPS = [
     'collectfast',
     'django.contrib.staticfiles',
     'backend.movie',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 ROOT_URLCONF = 'backend.urls'
 
