63338f36a64353fc4510ced83e0ad92cceb8d73c
alison <alison@metanoia>
2019-06-05 01:37:24 -0300

    Instalada e configurada Lib django_s3_folder_storage
    close #38

--- backend/settings.py
+++ backend/settings.py
@@ -130,3 +130,37 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+
+# STORAGE CONFIGURATION IN S3 AWS
+# ------------------------------------------------------------------------------
+
+if AWS_ACCESS_KEY_ID:
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')  # pragma: no cover
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')  # pragma: no cover
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }  # pragma: no cover
+    AWS_PRELOAD_METADATA = True  # pragma: no cover
+    AWS_AUTO_CREATE_BUCKET = False  # pragma: no cover
+    AWS_QUERYSTRING_AUTH = True  # pragma: no cover
+    AWS_S3_CUSTOM_DOMAIN = None  # pragma: no cover
+
+    AWS_DEFAULT_ACL = 'private'  # pragma: no cover
+
+    # Static Assets
+    # ------------------------------------------------------------------------------
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'  # pragma: no cover
+    STATIC_S3_PATH = 'static'  # pragma: no cover
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'  # pragma: no cover
+    STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'  # pragma: no cover
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'  # pragma: no cover
+
+    # Upload Media Folder
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'  # pragma: no cover
+    DEFAULT_S3_PATH = 'mediafiles'  # pragma: no cover
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'  # pragma: no cover
+    MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'  # pragma: no cover
+
+    INSTALLED_APPS.append('s3_folder_storage')  # pragma: no cover
+    INSTALLED_APPS.append('storages')  # pragma: no cover
