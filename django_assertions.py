31eaad1c251f4c493568ad1b622e5e47c30f4348
alison <alison@metanoia>
2019-06-05 18:07:30 -0300

    Criado teste para home da api
    close #52

---
+++ django_assertions.py
@@ -0,0 +1,11 @@
+"""
+Module to expose Django's TestCase assertions as functions
+"""
+
+
+from django.test import TestCase
+
+_dj_testcase = TestCase()
+
+dj_assert_contains = _dj_testcase.assertContains
+dj_assert_not_contains = _dj_testcase.assertNotContains
