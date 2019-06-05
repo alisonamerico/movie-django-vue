3455c7c1dfb4e1ee20a442685aa0e6c59df1372d
alison <alison@metanoia>
2019-06-05 18:48:17 -0300

    Configurada autenticação do usuário na api
    close #56

--- backend/movie/views.py
+++ backend/movie/views.py
@@ -2,7 +2,13 @@ from rest_framework import viewsets
 from .serializers import MovieSerializer
 from .models import Movie
 
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
+
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
