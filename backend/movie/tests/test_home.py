5d7d35605579edd88d01b1a7c89264298b3bff83
alison <alison@metanoia>
2019-06-10 02:32:26 -0300

    configurado campo de pesquisa
    close #77

--- backend/movie/tests/test_home.py
+++ backend/movie/tests/test_home.py
@@ -12,17 +12,17 @@ def home_resp(client):
     return _resp(client)
 
 
-# def _resp(client):
-#     """Função simples para evitar _pytest.warning_types.RemovedInPytest4Warning: Fixture "resp" chamado diretamente."""
-#     return client.get('/api/', secure=True)
-#
-#
-# @pytest.fixture
-# def home_resp_with_user(django_user_model, client: Client):
-#     user = mommy.make(django_user_model)  # pragma: no cover
-#     client.force_login(user)  # pragma: no cover
-#     return _resp(client)  # pragma: no cover
-#
-#
-# def test_home_status_code(home_resp):
-#     assert 200 == home_resp.status_code
+def _resp(client):
+    """Função simples para evitar _pytest.warning_types.RemovedInPytest4Warning: Fixture "resp" chamado diretamente."""
+    return client.get('/api/', secure=True)
+
+
+@pytest.fixture
+def home_resp_with_user(django_user_model, client: Client):
+    user = mommy.make(django_user_model)  # pragma: no cover
+    client.force_login(user)  # pragma: no cover
+    return _resp(client)  # pragma: no cover
+
+
+def test_home_status_code(home_resp):
+    assert 401 == home_resp.status_code
