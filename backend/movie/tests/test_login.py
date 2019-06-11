03fc8ba242594312604bd4df6a41aa591ea9a5a7
alison <alison@metanoia>
2019-06-11 09:41:18 -0300

    adicionado 'session' na autenticação
    close #88

--- backend/movie/tests/test_login.py
+++ backend/movie/tests/test_login.py
@@ -1,30 +1,15 @@
-# import pytest
-#
-# from django_assertions import dj_assert_contains
-#
-# """
-# Executa testes de validação do usuário.
-# """
-#
-#
-# @pytest.fixture
-# def resp(client, db):
-#     resp = client.get('/rest-auth/login', secure=True)
-#     return resp
-#
-#
-# def test_status_code(resp):
-#     assert resp.status_code == 200
-#
-#
-# # @pytest.mark.parametrize(
-# #     'content',
-# #     [
-# #         '<form',
-# #         '<input type="text" name="username"',
-# #         '<input type="password" name="password"',
-# #         'type="submit"',
-# #     ]
-# # )
-# # def test_page_content(content, resp):
-# #     dj_assert_contains(resp, content)
+import pytest
+
+"""
+Executa testes de validação do usuário.
+"""
+
+
+@pytest.fixture
+def resp(client, db):
+    resp = client.get('/rest-auth/login/', secure=True)
+    return resp
+
+
+def test_status_code(resp):
+    assert resp.status_code == 405
